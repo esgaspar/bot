160097f703907d80f1dcc60f23b92269ef40467d
Erick Gaspar <egaspar@sccalpha.local>
2019-08-21 23:10:42 -0300

    test and router fix

--- server.js
+++ server.js
@@ -6,9 +6,16 @@ var express = require('express'),
     Bot = require('./api/models/botModel'),
     bodyParser = require('body-parser');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/msgdb', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/botdb', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.disable('view cache');
+
+app.set('case sensitive routing', true);
+app.set('strict routing', true);
+app.set('view cache', false);
+app.set('x-powered-by', false);
+
 var routes = require('./api/routes/msgRoutes');
 routes(app);
 app.listen(port);
